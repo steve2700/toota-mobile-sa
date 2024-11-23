1fb8cf7107a5ba076189c685d5843d577e3b5ebb
Elizabeth-Panashe-Pfachi <testlolabtest@gmail.com>
2024-11-23 18:20:02 +0200

    Add basic User class

--- server/toota/authentication/models.py
+++ server/toota/authentication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=30, blank=False, null=False)
+    surname = models.CharField(max_length=40, blank=False, null=False)
+    number = models.IntegerField(unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=50)
+    residential_address = models.CharField(max_length=100)
+    profile_pic = models.CharField(max_length=255, unique=True)
+    
