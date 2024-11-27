f700cbd8b8fd21b82fb964aa74e098749b7678bc
Rihobot Elias <rihobotelias123@gmail.com>
2024-11-27 11:47:50 -0800

    sign up screen

--- toota_mobile_sa/lib/presentation/screens/splash_screen.dart
+++ toota_mobile_sa/lib/presentation/screens/splash_screen.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import '../screens/signup_screen.dart'; 
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const SignUpScreen()),
+      );
+    });
+
     return Scaffold(
       body: Center(
         child: Image.asset(
