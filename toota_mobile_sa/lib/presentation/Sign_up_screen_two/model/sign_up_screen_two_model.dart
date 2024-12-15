f2f8064e7718e10129db32240dc563d19145f6ef
Rihobot Elias <rihobotelias123@gmail.com>
2024-12-15 12:21:35 -0800

    added sign up screen two of verification page created models and block dart classes

---
+++ toota_mobile_sa/lib/presentation/Sign_up_screen_two/model/sign_up_screen_two_model.dart
@@ -0,0 +1,14 @@
+import 'package:equatable/equatable.dart';
+class SignUpScreenTwoModel extends Equatable
+ {
+   SignUpScreenTwoModel();
+
+   SignUpScreenTwoModel coppwith()
+   {
+    return SignUpScreenTwoModel();
+   }
+   @override
+   List<Object?> get props =>[];
+
+
+}
