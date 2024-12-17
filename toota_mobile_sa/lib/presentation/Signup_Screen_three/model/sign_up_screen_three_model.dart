7d63ae475f5b65f28822afd97549cd33a8b46095
Rihobot Elias <rihobotelias123@gmail.com>
2024-12-16 23:27:48 -0800

    added fourth screen of Sign up Screen

---
+++ toota_mobile_sa/lib/presentation/Signup_Screen_three/model/sign_up_screen_three_model.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+
+class SignUpScreenThreeModel extends Equatable
+ {
+  SignUpScreenThreeModel();
+  SignUpScreenThreeModel copywith()
+  {
+    return SignUpScreenThreeModel();
+  }
+  @override
+  List<Object?> get props => [];
+  
+ }
