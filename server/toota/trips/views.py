83c22db18c0717951e19bc7ef48fb4c38106fab3
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2025-01-21 14:26:05 +0200

    Update views.py

--- server/toota/trips/views.py
+++ server/toota/trips/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Trip
+from .serializers import TripSerializer
 
-# Create your views here.
+class CreateTripView(APIView):
+    def post(self, request):
+        serializer = TripSerializer(data=request.data)
+        if serializer.is_valid():
+            # Create the trip and set the initial status as "finding_driver"
+            trip = serializer.save(user=request.user, status='finding_driver')
+
+            # Return the trip ID so that the client can connect to the WebSocket
+            return Response({'trip_id': trip.id}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
