87089073e85754b016372c0ef36f8b4569b00418
Rihobot Elias <rihobotelias123@gmail.com>
2025-02-06 14:57:22 -0800

    to make changes in sign up

--- toota_mobile_sa/lib/routes.dart
+++ toota_mobile_sa/lib/routes.dart
@@ -6,18 +6,22 @@ import 'package:toota_mobile_sa/screens/role_screen.dart';
 import 'package:toota_mobile_sa/screens/Welcome%20Screen/welcome_screen.dart';
 import 'constants.dart';
 import 'screens/splash_screen.dart';
+import 'package:toota_mobile_sa/screens/sign_up_screen/sign_up_screen_one/sign_up_screen_one.dart';
 import 'package:toota_mobile_sa/screens/sign_up_screen/sign_up_screen.dart';
 
 // Define a function that returns the routes map
 Map<String, WidgetBuilder> getAppRoutes() {
   return {
     RouteNames.splash: (context) => const SplashScreen(),
-    RouteNames.signUp: (context) => const SignUpScreen(),
+    RouteNames.signUpOne: (context) =>  SignUpScreenOne(),
+    RouteNames.signUp: (context) => SignUpScreen(),
     RouteNames.role: (context) => const RoleScreen(),
     RouteNames.welcome: (context) => const WelcomeScreen(),
     RouteNames.onboarding: (context) =>  const OnboardingScreen(),
     RouteNames.kycone: (context) => const KycScreenOne(),
-     RouteNames.kyctwo: (context) => const KycScreenTwo(),
+    RouteNames.kyctwo: (context) => const KycScreenTwo(),
+
+     
     // Add more routes as needed
   };
 }
