d612ae31f170c6bfa879a07266623103851c89ef
Rihobot Elias <rihobotelias123@gmail.com>
2025-02-06 13:53:36 -0800

    removed duplicant and unneccassary files and define well defined structure for implementation and started it

--- toota_mobile_sa/lib/routes.dart
+++ toota_mobile_sa/lib/routes.dart
@@ -6,11 +6,13 @@ import 'package:toota_mobile_sa/screens/role_screen.dart';
 import 'package:toota_mobile_sa/screens/Welcome%20Screen/welcome_screen.dart';
 import 'constants.dart';
 import 'screens/splash_screen.dart';
+import 'package:toota_mobile_sa/screens/sign_up_screen/sign_up_screen.dart';
 
 // Define a function that returns the routes map
 Map<String, WidgetBuilder> getAppRoutes() {
   return {
     RouteNames.splash: (context) => const SplashScreen(),
+    RouteNames.signUp: (context) => const SignUpScreen(),
     RouteNames.role: (context) => const RoleScreen(),
     RouteNames.welcome: (context) => const WelcomeScreen(),
     RouteNames.onboarding: (context) =>  const OnboardingScreen(),
