190909a5149045c37c53321ace60bc3f98fb8bc8
shoile abdulazeez <shoabdulazeez@gmail.com>
2025-02-21 03:25:24 +0000

    update

--- server/toota/authentication/util/notifications.py
+++ server/toota/authentication/util/notifications.py
@@ -2,3 +2,981 @@ from django.core.mail import send_mail
 
 def send_notification(email, subject, message):
     send_mail(subject, message, 'admin@driververification.com', [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+, [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+', [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+, [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+m', [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+, [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+', [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+, [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+
+
+)
+
+[email])
+
+)
+
+', [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+, [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+m', [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+, [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+', [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+, [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+
+
+
+)
+
+[email])
+
+)
+
+', [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+, [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+m', [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+, [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+', [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+, [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+
+
+
+
+)
+
+[email])
+
+)
+
+', [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+, [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+m', [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+, [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+', [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+, [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+
+
+
+
+
+)
+
+[email])
+
+)
+
+', [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+, [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+m', [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+, [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+', [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+, [email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+mail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+mail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+mail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+mail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+mail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+mail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+
+])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+[email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+mail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+email])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+mail])
+
+)
+
+ [email])
+
+)
+
+[email])
+
+)
+
+ail])
+
+)
+
+
+
+
+[mi]
+
+
+)
+
+
+)
+
+a
+
+)
+
+[email])
+
+)
+
+
+
+
+)
+
