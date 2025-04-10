8c70b1775808aebed47a96ce484747ff0d7c382f
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2025-04-10 09:32:25 +0200

    Update urls.py

--- server/toota/trips/urls.py
+++ server/toota/trips/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
-from .views import (CheckTripStatusView, 
-                    WebSocketAvailableDriversDocView, WebSocketDriverTripStatusView, WebSocketDriverTripView,
-                    WebSocketTripRequestDocView)
+from .views import (
+    CheckTripStatusView,
+    WebSocketAvailableDriversDocView,
+    WebSocketTripRequestDocView,
+    WebSocketDriverTripStatusView,
+    WebSocketDriverTripView
+)
 
 urlpatterns = [
     path("<uuid:trip_id>/status/", CheckTripStatusView.as_view(), name="update-trip-status"),
+
+    # WebSocket Documentation Views
     path("docs/available-drivers/", WebSocketAvailableDriversDocView.as_view(), name="websocket-available-drivers"),
+    path("docs/trip-request/", WebSocketTripRequestDocView.as_view(), name="websocket-trip-request"),
     path("docs/driver-trip-status/", WebSocketDriverTripStatusView.as_view(), name="websocket-driver-trip-status"),
     path("docs/driver-trip/", WebSocketDriverTripView.as_view(), name="websocket-driver-trip"),
-    path("docs/trip-request/", WebSocketTripRequestDocView.as_view(), name="websocket-trip-request"),
 ]
